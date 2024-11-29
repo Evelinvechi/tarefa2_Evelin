cc3442f89fc91994a1652f16479d352369c3079b
Evelinvechi <evelin_rvechi@hotmail.com>
2024-11-29 10:57:07 +0000

    Alteração2

--- tarefa2_Evelin.py
+++ tarefa2_Evelin.py
@@ -2,3 +2,7 @@ def main():
     print("Olá, formador!")
     idade = input("Quantos anos você tem? ")
     print(f"Você tem {idade} anos. Muito bem!")
+    print("Eu tenho 34 anos!")
+    print("Eu moro em Lisboa.")
+cidade = input("Qual é a sua cidade? ")
+    print(f"Que legal! Você mora em {cidade}.")
